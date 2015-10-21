2f0641cd2ca5a48bbff02b77f3895fb8ef390bbd
vasyl <vasylvovkastr@gmail.com>
2015-10-21 23:46:38 +0300

    Initial commit

---
+++ src/main/java/org/ua/mk/vasylvovk/Task01.java
@@ -0,0 +1,11 @@
+package org.ua.mk.vasylvovk;
+
+/**
+ * Created by vasyl on 10/21/15.
+ */
+public class Task01 {
+    public static void main(String... args) {
+
+        System.out.println(args[0]+"\t"+args[1]);
+    }
+}
